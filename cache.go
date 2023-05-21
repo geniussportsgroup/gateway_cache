b4d2cf25d8225246cef211c058325cf0bd8a22e3
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2023-05-21 09:57:49 -0500

    Added a new field in RequestError for communicating user provided info to the invoker
    Implemented Touch, Contains, and LazyRemove methods

--- cache.go
+++ cache.go
@@ -44,7 +44,8 @@ type CacheEntry struct {
 type RequestError struct {
 	Error    error
 	Code     int
-	UserInfo interface{}
+	UserCode int
+	// TODO: add UserInfo as a kind of opaque data pointer. In that way, the user can return to the invoker any kind of additional data
 }
 
 type CacheDriver struct {
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/geniussportsgroup/gateway_cache
 
-go 1.15
+go 1.18
 
 require (
 	github.com/frankban/quicktest v1.14.3 // indirect
